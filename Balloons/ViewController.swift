7c03410492d92d3e2cdd3e23074e9e34685f6ce2
Gautam Babbar <gautambabbar@gmail.com>
2018-09-09 16:30:00 -0400

    Cocoa Pods and Ads in the dev environment

--- Balloons/ViewController.swift
+++ Balloons/ViewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 import SpriteKit
+import GoogleMobileAds
 
 class ViewController:UIViewController{
     
     var scene: AnimationScene!
     var size: CGSize!
+    var bannerView: GADBannerView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,7 +24,45 @@ class ViewController:UIViewController{
         
         let skView = self.view as! SKView
         skView.presentScene(scene)
+        
+        
+        
+        // In this case, we instantiate the banner with desired ad size.
+        bannerView = GADBannerView(adSize: kGADAdSizeBanner)
+        
+        addBannerViewToView(bannerView)
+        
+        
+        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        
+        
+        
     }
+    
+    func addBannerViewToView(_ bannerView: GADBannerView) {
+        bannerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(bannerView)
+        view.addConstraints(
+            [NSLayoutConstraint(item: bannerView,
+                                attribute: .bottom,
+                                relatedBy: .equal,
+                                toItem: bottomLayoutGuide,
+                                attribute: .top,
+                                multiplier: 1,
+                                constant: 0),
+             NSLayoutConstraint(item: bannerView,
+                                attribute: .centerX,
+                                relatedBy: .equal,
+                                toItem: view,
+                                attribute: .centerX,
+                                multiplier: 1,
+                                constant: 0)
+            ])
+    }
+    
+    
 }
 
 
